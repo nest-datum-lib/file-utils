49c36f1e28f4babb1f4b0e1006d132dd30efd898
ihor <ihor.bielchenko@gmail.com>
2023-01-27 22:07:42 +0100

    Create convert function.

---
+++ src/convert.js
@@ -0,0 +1,11 @@
+const fs = require('fs').promises;
+const libre = require('libreoffice-convert');
+
+(async () => {
+	libre['convertAsync'] = util.promisify(libre.convert);
+
+	const docxBuf = await fs.promises.readFile(process.argv[2]);
+	const pdfBuf = await libre['convertAsync'](docxBuf, '.pdf', undefined);
+
+	await fs.promises.writeFile(process.argv[2], pdfBuf);
+})();
